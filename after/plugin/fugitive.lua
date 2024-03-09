09dc0f3e11cba13de48aa8adb6a1b54d643845cb
Phillip Nguyen <png9981@gmail.com>
2024-03-09 12:50:38 +0700

    feat: adding command like git zsh for fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,27 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+vim.api.nvim_create_user_command('Gcmsg',function(opts)
+  vim.cmd("Git commit -m " .. opts.fargs[1])
+end,{
+  nargs = 1,
+})
+
+vim.api.nvim_create_user_command('Gp',function()
+  vim.cmd("Git push")
+end,{})
+
+vim.api.nvim_create_user_command('Gf',function()
+  vim.cmd("Git fetch")
+end,{})
+
+vim.api.nvim_create_user_command('Gl',function(opts)
+  local pullArgs = ""
+  for _, arg in ipairs(opts.fargs) do
+    print(arg)
+    pullArgs = pullArgs .. arg .. " "
+  end
+
+  vim.cmd("Git pull ".. pullArgs)
+end,{
+  nargs = "*"
+})
