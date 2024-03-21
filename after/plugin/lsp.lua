999eeb63400de5db1e9097207ebe43693f0deaf9
Phillip Nguyen <png9981@gmail.com>
2024-03-21 17:01:03 +0700

    feat: update lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,33 +1,33 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = { "tsserver", "lua_ls", "eslint", "terraformls", "dockerls", "docker_compose_language_service", "gopls", "golangci_lint_ls", "templ",
+  ensure_installed = { "tsserver", "lua_ls", "eslint", "dockerls", "docker_compose_language_service",
+    "gopls", "golangci_lint_ls", "templ",
     "tailwindcss"
   }
 })
+
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 local client_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-
 local on_attach = function(_, _)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder)
   vim.keymap.set('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  end)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename)
+  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references)
   vim.keymap.set('n', '<space>f', function()
-    print('format')
     vim.lsp.buf.format { async = true }
-  end, opts)
+  end)
 end
 
 lspconfig['tailwindcss'].setup {
@@ -38,18 +38,21 @@ lspconfig['tailwindcss'].setup {
 
 lspconfig.tsserver.setup {
   capabilities = client_capabilities,
-  on_attach = on_attach
+  on_attach = on_attach,
 }
 
 lspconfig.lua_ls.setup {
   capabilities = client_capabilities,
-  on_attach = on_attach
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
 }
 
-lspconfig.terraformls.setup {
-  capabilities = client_capabilities,
-  on_attach = on_attach
-}
 
 lspconfig['dockerls'].setup {
   capabilities = client_capabilities,
@@ -69,24 +72,24 @@ lspconfig.golangci_lint_ls.setup {
 lspconfig.templ.setup {
   capabilities = client_capabilities,
   on_attach = on_attach,
-  filetypes = {"templ"}
+  filetypes = { "templ" }
 }
 
 lspconfig.eslint.setup({
-	capabilities = client_capabilities,
-	flags = { debounce_text_changes = 500 },
-	on_attach = function(client, bufnr)
-		client.server_capabilities.documentFormattingProvider = true
-		if client.server_capabilities.documentFormattingProvider then
-      			local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
-      			vim.api.nvim_create_autocmd("BufWritePre", {
-        			pattern = "*",
-        			callback = function()
-        			end,
-        			group = au_lsp,
-      			})
-    		end
-  	end,
+  capabilities = client_capabilities,
+  flags = { debounce_text_changes = 500 },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = true
+    if client.server_capabilities.documentFormattingProvider then
+      local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function()
+        end,
+        group = au_lsp,
+      })
+    end
+  end,
 })
 
 vim.cmd([[
@@ -98,5 +101,3 @@ vim.cmd([[
 ]])
 
 vim.filetype.add({ extension = { templ = "templ" } })
-
-
