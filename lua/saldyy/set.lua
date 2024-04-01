c9151dbe65437f61d77afa8c74fd541a6f1b4e95
Phillip Nguyen <png9981@gmail.com>
2024-04-01 17:28:35 +0700

    feat: update remap and set

--- lua/saldyy/set.lua
+++ lua/saldyy/set.lua
@@ -1,9 +1,12 @@
 vim.opt.relativenumber = true
+vim.opt.number = true
 
 vim.opt.tabstop=2
 vim.opt.softtabstop=2
 vim.opt.shiftwidth=2
 vim.opt.expandtab = true
+
+vim.opt.autoread = true
 
 vim.opt.clipboard='unnamedplus'
 vim.opt.incsearch=false
