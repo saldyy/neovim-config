6be0987fdae1f82f021e7a42051e165810c73302
Phillip Nguyen <png9981@gmail.com>
2024-06-29 11:23:03 +0700

    feat: adding option

--- lua/saldyy/set.lua
+++ lua/saldyy/set.lua
@@ -1,16 +1,22 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
 
-vim.opt.tabstop=2
-vim.opt.softtabstop=2
-vim.opt.shiftwidth=2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+vim.autoindent = true
+vim.smartindent = true
 
 vim.opt.autoread = true
 
-vim.opt.clipboard='unnamedplus'
-vim.opt.incsearch=false
-vim.opt.hlsearch=false
+vim.opt.wrap = true
+vim.opt.textwidth = 100
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.incsearch = false
+vim.opt.hlsearch = false
 
 vim.cmd([[
   augroup highlight_yank
