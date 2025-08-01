960609e006f4b09aeb8709f47e4e73125f7cf62d
Phillip Nguyen <png9981@gmail.com>
2025-08-01 11:43:02 +0700

    feat: update grep for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,25 +1,72 @@
-local builtin = require('telescope.builtin')
-local telescope = require('telescope')
+local builtin = require("telescope.builtin")
+local telescope = require("telescope")
 
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local live_multigrep = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.fn.getcwd()
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      local r = vim.tbl_flatten {
+        args,
+        { "--column", "--color=never", "--no-heading", "--line-number", "--with-filename", "--smart-case", }
+      }
+      print (r)
+      return r
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers.new(opts, {
+    debounce = 100,
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty(),
+    prompt_title = "Live MultiGrep",
+  }):find()
+end
 
 telescope.setup {
   defaults = {
-    sorting_strategy = 'ascending',
-    selection_strategy = 'follow',
-    layout_strategy = 'vertical',
+    sorting_strategy = "ascending",
+    selection_strategy = "follow",
+    layout_strategy = "vertical",
     layout_config = {
       height = 0.8,
       width = 0.9,
-      prompt_position = 'bottom',
+      prompt_position = "bottom",
       preview_height = 0.6
     }
+  },
+  extensions = {
+    fzf = {}
   }
 }
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-  -- builtin.grep_string({ search = vim.fn.input("Grep > ") });
-  builtin.live_grep({ prompt_title = "Live Grep > " });
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+pcall(telescope.load_extension, "fzf")
+
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ps", live_multigrep, {})
+vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
